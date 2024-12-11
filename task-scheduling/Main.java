f0bd4a1cc92e987699d43f440b09ca393afb0792
foreverwhb-ustb <59986168+foreverwhb-ustb@users.noreply.github.com>
2024-12-11 11:47:57 +0800

    Update Main.java

--- task-scheduling/Main.java
+++ task-scheduling/Main.java
@@ -37,45 +37,45 @@ public class Main {
 
     }
 
-//    public static void createTestData(String filePath) {
-//        //create 50,000 data as cloudlet length for subsequent testing.
-//        int taskNum = 300;
-//        int[] taskLength = new int[taskNum];
-//        for (int i = 0; i < taskNum; i++) {
-////            taskLength[i] = (new Random().nextInt(200) + 1) * 50 + new Random().nextInt(500);
-//            taskLength[i] = new Random().nextInt(500, 1500);
-//        }
-//
-//        StringBuilder sb = new StringBuilder();
-//        for (int i = 0; i < taskNum; i++) {
-//            sb.append(taskLength[i]).append("\t");
-//            if (i % 50 == 49)//20 data each line.
-//            {
-//                CloudSimExe1.writeTxtAppend(filePath, sb.toString());
-//                sb = null;
-//                sb = new StringBuilder();
-//            }
-//        }
-//    }
-//    public static void createTestData2() {
-//        int i = 500;
-//        int index = 1;
-//        String filePath = "data\\cloudlets.txt";
-//        StringBuilder sb = new StringBuilder();
-//        while (i <= 515) {
-//            int j = i;
-//            while (j < 1400) {
-//                sb.append(j).append("\t");
-//                j += 18;
-//                if (index % 50 == 0) {
-//                    CloudSimExe1.writeTxtAppend(filePath, sb.toString());
-//                    sb = null;
-//                    sb = new StringBuilder();
-////                    System.out.println();
-//                }
-//                index++;
-//            }
-//            i += 3;
-//        }
-//    }
+   public static void createTestData(String filePath) {
+       //create 50,000 data as cloudlet length for subsequent testing.
+       int taskNum = 300;
+       int[] taskLength = new int[taskNum];
+       for (int i = 0; i < taskNum; i++) {
+//            taskLength[i] = (new Random().nextInt(200) + 1) * 50 + new Random().nextInt(500);
+           taskLength[i] = new Random().nextInt(500, 1500);
+       }
+
+       StringBuilder sb = new StringBuilder();
+       for (int i = 0; i < taskNum; i++) {
+           sb.append(taskLength[i]).append("\t");
+           if (i % 50 == 49)//20 data each line.
+           {
+               CloudSimExe1.writeTxtAppend(filePath, sb.toString());
+               sb = null;
+               sb = new StringBuilder();
+           }
+       }
+   }
+   public static void createTestData2() {
+       int i = 500;
+       int index = 1;
+       String filePath = "data\\cloudlets.txt";
+       StringBuilder sb = new StringBuilder();
+       while (i <= 515) {
+           int j = i;
+           while (j < 1400) {
+               sb.append(j).append("\t");
+               j += 18;
+               if (index % 50 == 0) {
+                   CloudSimExe1.writeTxtAppend(filePath, sb.toString());
+                   sb = null;
+                   sb = new StringBuilder();
+//                    System.out.println();
+               }
+               index++;
+           }
+           i += 3;
+       }
+   }
 }
